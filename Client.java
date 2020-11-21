8efcaf55d7cf11950f3bdd399442c80df1691cf2
srubey <scrubey@pdx.edu>
2020-11-21 13:38:09 -0800

    Client can handle server being offline upon program start.

--- Client.java
+++ Client.java
@@ -17,87 +17,86 @@ public class Client {
 
             //connect to server
             boolean connected = connect("127.0.0.1", 6789);
-            if(connected)
+            if(connected) {
                 System.out.println("Connected to server");
-            else
-                System.out.println("Error connecting to server");
 
-            // obtaining input and out streams
-            inputStream = new DataInputStream(clientSocket.getInputStream());
-            outputStream = new DataOutputStream(clientSocket.getOutputStream());
+                // obtaining input and out streams
+                inputStream = new DataInputStream(clientSocket.getInputStream());
+                outputStream = new DataOutputStream(clientSocket.getOutputStream());
 
-            boolean disc = false;  //user does not presently wish to disconnect
-            int choice = Menu.topMenu();
+                boolean disc = false;  //user does not presently wish to disconnect
+                int choice = Menu.topMenu();
 
-            //while user wishes to stay connected
-            while (!disc){
-                String command = null;
+                //while user wishes to stay connected
+                while (!disc) {
+                    String command = null;
 
-                //if user opts to list existing rooms
-                if (1 == choice){
-                    command = username + " LIST";
-                }
-                //if user opts to join an existing room
-                else if (2 == choice){
-                    System.out.println("Please enter the name of the room you would like to join: ");
-                    String roomName = scanner.next();
+                    //if user opts to list existing rooms
+                    if (1 == choice) {
+                        command = username + " LIST";
+                    }
+                    //if user opts to join an existing room
+                    else if (2 == choice) {
+                        System.out.println("Please enter the name of the room you would like to join: ");
+                        String roomName = scanner.next();
 
-                    command = username + " JOIN " + roomName;
-                }
-                //if user opts to create a new room
-                else if (3 == choice) {
-                    System.out.print("Please enter the name of the room you would like to create (no spaces allowed): ");
-                    String newRoomName = scanner.next();
+                        command = username + " JOIN " + roomName;
+                    }
+                    //if user opts to create a new room
+                    else if (3 == choice) {
+                        System.out.print("Please enter the name of the room you would like to create (no spaces allowed): ");
+                        String newRoomName = scanner.next();
 
-                    command = username + " CTRM " + newRoomName;
-                }
-                //if user opts to list all users in a chat room
-                else if (4 == choice){
-                    System.out.print("Please enter the name of the room whose members you would like to list: ");
-                    String roomName = scanner.next();
+                        command = username + " CTRM " + newRoomName;
+                    }
+                    //if user opts to list all users in a chat room
+                    else if (4 == choice) {
+                        System.out.print("Please enter the name of the room whose members you would like to list: ");
+                        String roomName = scanner.next();
 
-                    command = username + " LSMB " + roomName;
-                }
-                //if user opts to post a msgText to a chat room
-                else if (5 == choice){
-                    System.out.print("Please enter the name of the room you would like to post to: ");
-                    String roomName = scanner.next();
-                    scanner.nextLine();
+                        command = username + " LSMB " + roomName;
+                    }
+                    //if user opts to post a msgText to a chat room
+                    else if (5 == choice) {
+                        System.out.print("Please enter the name of the room you would like to post to: ");
+                        String roomName = scanner.next();
+                        scanner.nextLine();
 
-                    System.out.print("Please enter your message: ");
-                    String message = scanner.nextLine();
+                        System.out.print("Please enter your message: ");
+                        String message = scanner.nextLine();
 
-                    command = username + " POST " + roomName + " " + message;
-                }
-                //if user opts to retrieve all messages posted to a chat room
-                else if (6 == choice){
-                    System.out.print("Please enter the name of the room whose messages you would like shown: ");
-                    String roomName = scanner.next();
+                        command = username + " POST " + roomName + " " + message;
+                    }
+                    //if user opts to retrieve all messages posted to a chat room
+                    else if (6 == choice) {
+                        System.out.print("Please enter the name of the room whose messages you would like shown: ");
+                        String roomName = scanner.next();
 
-                    command = username + " RETV " + roomName;
-                }
-                //if user opts to leave a chat room
-                else if (7 == choice){
-                    System.out.print("Please enter the name of the room you would like to leave: ");
-                    String roomName = scanner.next();
+                        command = username + " RETV " + roomName;
+                    }
+                    //if user opts to leave a chat room
+                    else if (7 == choice) {
+                        System.out.print("Please enter the name of the room you would like to leave: ");
+                        String roomName = scanner.next();
 
-                    command = username + " LEAV " + roomName;
-                }
-                //if user opts to disconnect from server
-                else if (8 == choice){
-                    command = username + " DSCT";
-                    disc = true;
-                }
+                        command = username + " LEAV " + roomName;
+                    }
+                    //if user opts to disconnect from server
+                    else if (8 == choice) {
+                        command = username + " DSCT";
+                        disc = true;
+                    }
 
-                //send command to server
-                outputStream.writeUTF(command);
+                    //send command to server
+                    outputStream.writeUTF(command);
 
-                //if the user hasn't disconnected, read the return message
-                if(!disc) {
-                    String retMsg = inputStream.readUTF();
-                    System.out.println(msgHandler(retMsg));
+                    //if the user hasn't disconnected, read the return message
+                    if (!disc) {
+                        String retMsg = inputStream.readUTF();
+                        System.out.println(msgHandler(retMsg));
 
-                    choice = Menu.topMenu();
+                        choice = Menu.topMenu();
+                    }
                 }
             }
 
